dec58edd7554dd0081f7e60f1cb508775506afd0
CodePoint-46615 <codepoint46615@gmail.com>
2025-08-19 22:34:25 +0600

    initial-project-commit

--- src/host/updateExhibition.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateExhibitionDto } from './createExhibition.dto';
-
-export class UpdateExhibitionDto extends PartialType(CreateExhibitionDto) {}
-
-
